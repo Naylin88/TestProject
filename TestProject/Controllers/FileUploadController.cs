384770f2820693e5675ab02f0e5fc463763519df
Nay Lin Oo <naylinoo2009@gmail.com>
2022-04-30 19:27:57 +0630

    Add project files.

---
+++ TestProject/Controllers/FileUploadController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestProject.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        private readonly ILogger<FileUploadController> _logger;
+
+        public FileUploadController(ILogger<FileUploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+            return View();
+        }
+
+      
+    }
+}
